1149d81b1a8fbf2ebbc250bdd1e1ab3a0d7efcd9
khbash <khrystynabash09@gmail.com>
2026-02-24 11:26:13 +0200

    start 02

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 import requests
+from pyexpat import features
+
 response = requests.get("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson")
 
 URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson"
 
 data=response.json()
+
+import json
+with open('earthquake.json', 'w', encoding="utf-8") as file:
+    json.dump(data, file, indent=5, ensure_ascii=False)
+
+PARAMS = {
+    "Place" :
+    "Magnitude" :
+    "Time" :
+    "Depth" :
+}
